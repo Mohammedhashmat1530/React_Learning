f78eeeb564371491c0dacb30d21125fae30fc0c3
hashmat1530 <hashmat2020vskp@gmail.com>
2024-01-13 05:22:20 +0530

    Understanding JSX

---
+++ 03_Understanding_jsx/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello hash (full stack developer)</h1>
+    <div className="blank">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem veniam voluptates enim, possimus ab repellat recusandae, corporis nam reiciendis hic officia dolor tempore placeat soluta modi obcaecati debitis suscipit facilis cum asperiores esse? Eveniet ipsam iure tempora ipsa molestias officiis aperiam commodi, voluptatibus dolore rem!
+    </div>
+
+    <ul>
+      <li>nodejs</li>
+      <li>ExpessJs</li>
+      <li>ReactJs</li>
+      <li>MongoDB</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
