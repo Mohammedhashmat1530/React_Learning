20c7a1c4b1ef5c6a67a8c98e62056efb9b6082f1
hashmat1530 <hashmat2020vskp@gmail.com>
2024-01-14 03:35:10 +0530

    refinalizing 03_Understanding_jsx

--- 03_Understanding_jsx/my-app/src/App.js
+++ 03_Understanding_jsx/my-app/src/App.js
@@ -1,22 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name="hash"
+
+const headerStyles = {
+  backgroundColor: '#61DBFB',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5
+}
+
+const header = (
+  <header style={headerStyles}>
+    <h1>Welcome to 30 Days Of React</h1>
+    <h2>Getting Started React</h2>
+    <h3>JavaScript Library</h3>
+    <p>Asabeneh Yetayeh</p>
+    <small>Oct 2, 2020</small>
+  </header>
+)
+
+const main = (
+  <main>
+    <p>Prerequisite to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>{name}</li>
+    </ul>
+  </main>
+)
+
+// JSX element, footer
+const footer = (
+  <footer style={headerStyles}>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+const footer2 = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+const footer3 = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+
+
 function App() {
   return (
-    <>
-    <h1>Hello hash (full stack developer)</h1>
-    <div className="blank">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem veniam voluptates enim, possimus ab repellat recusandae, corporis nam reiciendis hic officia dolor tempore placeat soluta modi obcaecati debitis suscipit facilis cum asperiores esse? Eveniet ipsam iure tempora ipsa molestias officiis aperiam commodi, voluptatibus dolore rem!
-    </div>
-
-    <ul>
-      <li>nodejs</li>
-      <li>ExpessJs</li>
-      <li>ReactJs</li>
-      <li>MongoDB</li>
-    </ul>
-    </>
-  );
+    <div>
+    {header}
+    {main}
+    {footer}
+    {footer2}
+    {footer3}
+  </div>
+  )
 }
 
 export default App;
